0bf3d4fd323a5a087ca631db27fec8988e066656
Blake <bleonard252@rocketmail.com>
2020-10-26 00:51:17 -0400

    Add UUID support to learn

--- src/learn.rs
+++ src/learn.rs
@@ -19,6 +19,7 @@ use num_traits::{ToPrimitive, Zero};
 use std::convert::TryInto;
 use chrono;
 use chrono::TimeZone;
+use uuid::Uuid;
 
 pub fn learn(id: &str) {
     // "Negative one" indicates an invalid numeric ID here.
@@ -29,18 +30,34 @@ pub fn learn(id: &str) {
         Ok(x) => {numid = x; is_numid = true;}
         Err(_) => {numid = BigUint::zero(); is_numid = false}
     }
-    if id.len() <= 21 && id.len() >= 10 && is_numid {
-        //Assume it might be a snowflake
-        let nid = numid.to_i128().unwrap();
-        let tsnum = (nid >> 22) + 1420070400000;
-        let workid = (nid & 0x3E0000) >> 17;
-        let procid = (nid & 0x1F000) >> 12;
-        let seqid = nid & 0xFFF;
-        //let tsdur = std::time::Duration::from_millis(tsnum.try_into().unwrap());
-        //let tsdur = chrono::Duration::milliseconds(tsnum.try_into().unwrap());
-        let tsutc = chrono::Utc.timestamp_millis(tsnum.try_into().unwrap());
-        let tsloc = chrono::Local.timestamp_millis(tsnum.try_into().unwrap());
-        println!("-----\nPotential type: Discord Snowflake\nTime: {}\nTime: {}\nWorker ID: {}\nProcess ID: {}\nSequence number: {}", 
-            tsutc, tsloc, workid, procid, seqid)
+    if id.len() <= 21 && id.len() >= 10 && is_numid {println!("{}", snowflake(id, numid));}
+    match Uuid::parse_str(id) {
+        Ok(x) => {
+            println!("{}", uuidlearn(x));
+        }
+        Err(_) => {} //do nothing
     }
+}
+
+fn snowflake(_id: &str, numid: BigUint) -> String {
+    //Assume it might be a snowflake
+    let nid = numid.to_i128().unwrap();
+    let tsnum = (nid >> 22) + 1420070400000;
+    let workid = (nid & 0x3E0000) >> 17;
+    let procid = (nid & 0x1F000) >> 12;
+    let seqid = nid & 0xFFF;
+    let tsutc = chrono::Utc.timestamp_millis(tsnum.try_into().unwrap());
+    let tsloc = chrono::Local.timestamp_millis(tsnum.try_into().unwrap());
+    return format!("-----\nPotential type: Discord Snowflake\nTime: {}\nTime: {}\nWorker ID: {}\nProcess ID: {}\nSequence number: {}", 
+        tsutc, tsloc, workid, procid, seqid)
+}
+
+fn uuidlearn(id: Uuid) -> String {
+    let mut result: String = "".to_owned();
+    let vn = id.get_version_num();
+    result += &format!("-----\nType: UUID\nVersion: {}",
+        vn);
+    //if there's anything else we can determine from a UUID,
+    //get it here
+    return result;
 }
